248e167a14d04f6ec0b8881a84da0e96e27fe109
arnmary <marinaarnaut482@gmail.com>
2025-06-06 18:57:45 +0300

    0506062025

--- src/components/SuccessPage.jsx
+++ src/components/SuccessPage.jsx
@@ -4,13 +4,11 @@ import { Link } from 'react-router-dom';
 
 export default function SuccessPage() {
   return (
-    <div style={{
-      display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-      height: '100vh', backgroundColor: '#f2f2f2', color: '#333', textAlign: 'center'
-    }}>
-      <h1 style={{ color: '#4CAF50' }}>Thank you!</h1>
-      <p>Your message was successfully sent.</p>
-      <Link to="/">← Back to Home</Link>
+    <div className='successBlock'>
+      <h2
+       >Thank you!</h2>
+      <p className='simpleSecondText'>Your message was successfully sent.</p>
+      <Link to="/" className='simpleBtn simpleSecondText'>← Back to Home</Link>
     </div>
   );
 }
